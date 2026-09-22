5ad546b5dfd51c671b638868f895a8cfd6395247
Mohamed Faizal <42328708+TeaMakesMePee@users.noreply.github.com>
2026-09-22 22:02:04 +0800

    Move Terraform state to S3 and locking

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
             version = "~> 5.0"
         }
     }
+    backend "s3" {
+      bucket         = "bearbard-tfstate-faizal"
+      key            = "bearbard/terraform.tfstate"
+      region         = "ap-southeast-1"
+      use_lockfile = true
+      encrypt        = true
+    }
 }
 
 provider "aws" {
