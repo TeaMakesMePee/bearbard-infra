14c19a5181a84a9187bb3db59809da13a14e6d3f
Mohamed Faizal <42328708+TeaMakesMePee@users.noreply.github.com>
2026-09-22 22:03:53 +0800

    ec2.tf modigied

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "bot_server" {
-    ami = data.aws_ami.ubuntu.id
+    ami = "ami-0ed6a65b84536f6ce"
     instance_type = var.instance_type
     key_name = var.key_name
     vpc_security_group_ids = [aws_security_group.bot_sg.id]
+    iam_instance_profile   = "bearbard-cloudwatch-role"
 
     tags = {
         Name = "bearbard-bot-prod"
